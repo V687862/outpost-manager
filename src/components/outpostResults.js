14c0b95ed7eab3508e5a0770fa470c56214148f9
Vernon Leadbeater <vernon.leadbeater@gmail.com>
2023-10-05 00:50:10 -0400

    outpostResults file update

--- src/components/outpostResults.js
+++ src/components/outpostResults.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import {canProduceGood, getOutpostName} from "../redux/actions";
-import {formatGoodNames} from "../Utilities/resourceutils"; // or utils
+import {formatGoodNames} from "../Utilities/resourceutils";
 
 const OutpostResultComponent = ({outpost}) => {
-    const outpostName = useSelector(state => {
-        console.log('State:', state);
-        return getOutpostName(state, outpost?.id);
-    });
-    const manufacturableGoods = useSelector(state => {
-        canProduceGood(state, outpost.id)
-    });
+    const dispatch = useDispatch();
+
+    // Dispatch actions to fetch data
+    useEffect(() => {
+        if (outpost && outpost.id != null) {
+            dispatch(getOutpostName(outpost.id));
+            dispatch(canProduceGood(outpost.id));
+        }
+    }, [dispatch, outpost]);
+
+    // Use selectors to get data from the Redux store
+    const outpostName = useSelector(state => state.outpost.outpostName);
+    const manufacturableGoods = useSelector(state => state.resource.canProduceGood); // Adjust according to your state structure
+
     return (
         <div>
             <h3>{outpostName}</h3>
-            {manufacturableGoods.length > 0
+            {manufacturableGoods && manufacturableGoods.length > 0
                 ? <>Can manufacture: {formatGoodNames(manufacturableGoods)}</>
                 : <>Cannot manufacture any goods with current resources.</>
             }
         </div>
     );
 };
+
 OutpostResultComponent.defaultProps = {
     outpost: {},
 };
